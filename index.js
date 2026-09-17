87ed6ff46b9ea67aad39550f6afff0c5634196cc
Toa <92904680+Paorapotangaroa@users.noreply.github.com>
2026-09-17 11:27:30 -0600

    added a get route

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const PORT = 3000;
 app.use(express.urlencoded({extended:true}));
 
 
+app.get("/projects/new",(req,res)=>{
+    res.send("Send the create project page");
+});
+
+
+
 // Start listening
 app.listen(PORT,()=>{
     console.log(`App is live: http://localhost:${PORT}`)
