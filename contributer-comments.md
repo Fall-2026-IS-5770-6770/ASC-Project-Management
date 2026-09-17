ba647ad79bb19b4b89cb354c7ee4eecfa7b3e510
Derrik Miller <derriklmiller@gmail.com>
2026-09-17 17:30:03 +0000

    Adding contributer comments. Added get, post, delete routes.

--- contributer-comments.md
+++ contributer-comments.md
@@ -3,3 +3,5 @@ Put your comments into this file with your GitHub username. This is a public rep
 Exmaple:
 @Paorapotangaroa: "Any application that can be written in JavaScript, will eventually be written in JavaScript." - Jeff Atwood
 
+@RikoSwave: "If... If is good." - Panic, Disney's Hercules
+
--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // Import required software
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 3030;
 
 // Allow body encoding for POST Requests
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/projects/new", (req, res) => {
+    res.send("Send the create project page.")
+})
+
+app.post("/projects/new", (req, res) => {
+    console.log(req.body);
+    res.send("Saving a new project.");
+})
+
+app.get("/projects/edit/:id", (req, res) => {
+    res.send("Edit a project page:" + req.params.id);
+})
+
+app.post("/projects/edit/:id", (req, res) => {
+    res.send("Saving the changes to project:" + req.params.id);
+})
+
+app.delete("/projects/delete/:id", (req, res) => {
+    res.send("Deleting a project:" + req.params.id);
+})
 
 // Start listening
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`App is live: http://localhost:${PORT}`)
 })
