7f964942777a64962b17ba2afd994cb81e98d5aa
ellieangus <ellie.angus724@gmail.com>
2026-09-17 11:27:46 -0600

    Added a git route and contributer-comment

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const PORT = 3000;
 // Allow body encoding for POST Requests
 app.use(express.urlencoded({extended:true}));
 
+app.get("/projects/new", (req, res) => {
+    res.send("Send the create project page");
+});
+
+app.post("/projects/new", (req, res) => {
+    console.log(req.body);
+    res.send("Saving a new project");
+});
+
+app.get("/projects/edit/:id", (req, res) => {
+    res.send("Edit specific project"+req.params.id);
+});
+
+app.post("/projects/edit/:id", (req, res) => {
+    console.log(req.body);
+    res.send("Updating project with ID "+req.params.id);
+});
+
 
 // Start listening
 app.listen(PORT,()=>{
