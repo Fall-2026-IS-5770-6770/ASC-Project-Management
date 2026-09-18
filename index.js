852ee2db3641e4cbfa169a1e9b2e1d2a5688e970
Toa <92904680+Paorapotangaroa@users.noreply.github.com>
2026-09-18 18:15:17 +0000

    Implement CRUD routes for project management

--- index.js
+++ index.js
@@ -7,10 +7,47 @@ const PORT = 3000;
 app.use(express.urlencoded({extended:true}));
 
 
+// CREATE
+// Get the create project page
 app.get("/projects/new",(req,res)=>{
     res.send("Send the create project page");
 });
 
+// Save the new project from the create form
+app.post("/projects/new",(req,res)=>{
+    res.send("Save the new project");
+});
+
+
+// READ
+// Get all projects
+app.get("/projects",(req,res)=>{
+    res.send("Send all of the projects");
+});
+
+// Get one project by id
+app.get("/projects/:id",(req,res)=>{
+    res.send(`Send project ${req.params.id}`);
+});
+
+
+// UPDATE
+// Get the edit page for one project
+app.get("/projects/:id/edit",(req,res)=>{
+    res.send(`Send the edit page for project ${req.params.id}`);
+});
+
+// Save the edit form for one project
+app.post("/projects/:id/edit",(req,res)=>{
+    res.send(`Save the edits to project ${req.params.id}`);
+});
+
+
+// DELETE
+// Delete one project by id
+app.post("/projects/:id/delete",(req,res)=>{
+    res.send(`Delete project ${req.params.id}`);
+});
 
 
 // Start listening
