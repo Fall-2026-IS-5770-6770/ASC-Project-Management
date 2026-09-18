d8af242672e88e6c8a6daa0c69f5e70ebe0abd6c
Michael Teeples <michaeltoddteeples@gmail.com>
2026-09-18 12:46:27 +0000

    Fix order and route names
    The "/person-skill/all" route must come before "/person-skill/:id". Otherwise "all" is interpreted as an id instead of part of a separate endpoint

--- index.js
+++ index.js
@@ -17,24 +17,24 @@ app.post("/person-skill/new", (req, res) => {
     res.send("Save new person-skill association");
 });
 
-app.get("/person-skill/:id", (req, res) => {
-    res.send(`Page to view person-skill association with id ${req.params.id}`);
-});
-
 app.get("/person-skill/all", (req, res) => {
     res.send("Page to view all person-skill associations");
 });
 
-app.get("person-skill/edit/:id", (req, res) => {
+app.get("/person-skill/:id", (req, res) => {
+    res.send(`Page to view person-skill association with id ${req.params.id}`);
+});
+
+app.get("/person-skill/edit/:id", (req, res) => {
     res.send(`Page to edit person-skill association with id ${req.params.id}`);
 });
 
-app.post("person-skill/edit/:id", (req, res) => {
+app.post("/person-skill/edit/:id", (req, res) => {
     console.log(req.body) // Data for edit
     res.send(`Save edit to person-skill association with id ${req.params.id}`);
 });
 
-app.post("person-skill/delete/:id", (req, res) => {
+app.post("/person-skill/delete/:id", (req, res) => {
     res.send(`Delete person-skill association with id ${req.params.id}`);
 });
 
