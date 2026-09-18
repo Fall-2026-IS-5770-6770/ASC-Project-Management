6375bb16329b2f0dcd08d36f7ceb5a156c0d55e7
brooklynwagner3 <a02358954@usu.edu>
2026-09-18 12:10:58 -0600

    hw 3 - get/post

--- index.js
+++ index.js
@@ -7,7 +7,44 @@ const PORT = 3000;
 app.use(express.urlencoded({extended:true}));
 
 
+// create project skills
+app.get("/project-skills/create",(req,res)=>{
+    res.send("create project skills");
+});
+
+app.post("/project-skills",(req,res)=>{
+    res.send("new project skills saved");
+});
+
+
+// view the skills 
+app.get("/project-skills", (req,res) => {
+    res.send("view project skills");
+});
+
+app.get("/project-skills/:id",(req,res)=>{
+    res.send(`view project skills with id: ${req.params.id}`);
+});
+
+
+// update skills 
+app.get("/project-skills/:id/edit",(req,res)=>{
+    res.send(`edit project skills with id: ${req.params.id}`);
+});
+
+app.post("/project-skills/:id/edit",(req,res)=>{
+    res.send(`project skills with id: ${req.params.id} updated`);
+});
+
+
+// delete skills 
+app.get("/project-skills/:id/delete",(req,res)=>{
+    res.send(`delete project skills with id: ${req.params.id}`);
+});
+
+
 // Start listening
 app.listen(PORT,()=>{
     console.log(`App is live: http://localhost:${PORT}`)
 })
+
