3fd3d229552de40a0613b5f4b818eda195b29a8b
lukeni64 <lukeni0064@gmail.com>
2026-09-19 11:30:29 -0600

    submission for HW3

--- index.js
+++ index.js
@@ -7,6 +7,44 @@ const PORT = 3000;
 app.use(express.urlencoded({extended:true}));
 
 
+//C - create a new person
+app.get("/people/new", (req,res)=>{
+    res.send("Create a new person");
+})
+
+app.post("/people/new", (req,res)=>{
+    console.log(req.body);
+    res.send("Saving a new person");
+})
+
+
+//R - view all/view a new person
+app.get("/people/view/:id", (req,res)=>{
+    res.send(`View a specific person with id: ${req.params.id}`);
+})
+
+app.get("/people/view", (req,res)=>{
+    res.send("View all people");
+})
+
+
+//U - edit a person
+app.get("/people/edit/:id", (req,res)=>{
+    res.send(`Edit a person with id: ${req.params.id}`);
+})
+
+app.post("/people/edit/:id", (req,res)=>{
+    console.log(req.body)
+    res.send(`Saving edits on a person with id: ${req.params.id}`)
+})
+
+//D - delete a person
+app.post("/people/delete/:id", (req,res)=>{
+    console.log(req.body);
+    res.send(`Deleting a person with id: ${req.params.id}`);
+})
+
+
 // Start listening
 app.listen(PORT,()=>{
     console.log(`App is live: http://localhost:${PORT}`)
