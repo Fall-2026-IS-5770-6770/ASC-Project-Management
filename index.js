85d3436af3ebedbb02eba18995ef7f65c88cc187
Derrik Miller <derriklmiller@gmail.com>
2026-09-18 21:27:06 -0600

    Updated index.js to be able to track project types.

--- index.js
+++ index.js
@@ -4,10 +4,35 @@ const app = express();
 const PORT = 3000;
 
 // Allow body encoding for POST Requests
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/project/type', (req, res) => {
+    res.send("List of the available types of Projects")
+})
+
+app.get('/project/type/create', (req, res) => {
+    res.send("Create Project type")
+})
+
+app.post('/project/type/create', (req, res) => {
+    res.send("Project type created")
+})
+
+app.get('/project/type/:id', (req, res) => {
+    res.send("Project type details")
+})
+
+app.put('/project/type/:id', (req, res) => {
+    res.send("Project type updated")
+})
+
+app.delete('/project/type/:id', (req, res) => {
+    res.send("Delete Project type")
+})
+
 
 
 // Start listening
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`App is live: http://localhost:${PORT}`)
 })
