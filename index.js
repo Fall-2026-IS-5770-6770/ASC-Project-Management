8ca7696d3c3c288fad490898affcc11c24f39ad6
g-howard <garick.howard@gmail.com>
2026-09-20 03:51:59 +0000

    Committing route setup for mentors

--- index.js
+++ index.js
@@ -6,6 +6,40 @@ const PORT = 3000;
 // Allow body encoding for POST Requests
 app.use(express.urlencoded({extended:true}));
 
+app.get("/mentors/new", (req, res) => {
+    res.send("Create mentors page")
+});
+
+app.post("/mentors/new", (req, res) => {
+    console.log(req.body);
+    res.send("Saving a new mentor");
+});
+
+
+
+app.get("/mentors/:id", (req,res) => {
+    res.send("getting a mentor " + req.params.id)
+});
+
+app.get("/mentors", (req, res) => {
+    res.send("get all mentors");
+});
+
+
+
+app.get("/mentors/edit", (req, res) => {
+    res.send("Edit a Mentor Page");
+})
+app.post("/mentors/edit", (req, res) => {
+    console.log(res.body);
+    res.send("Saving an edit on a mentor");
+})
+
+app.post("mentors/:id/delete", (req, res) => {
+    console.log(res.body);
+    res.send("deleting a mentor by Id: " + req.params.id);
+})
+
 
 // Start listening
 app.listen(PORT,()=>{
