4c80fd18eca422492f83802dc6670e3975f16576
RikoSwave <derriklmiller@gmail.com>
2026-09-24 17:35:03 +0000

    adding in class notes.

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ let app = express();
 const PORT = 3000;
 
 // Allow body encoding for POST Requests
-app.use(express.urlencoded({extended:true}));
-// TASK 12: TRACKIN PEOPLE (MENTORS/STUDENTS) ASSOCIATED WITH PROJECTS
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 // viewing all
 app.get("/projects/:projectid/people", (req, res) => {
@@ -46,75 +47,75 @@ app.post("/projects/:projectid/people/:relationshipid/delete", (req, res) => {
 
 // CREATE
 // Get the create project page
-app.get("/projects/new",(req,res)=>{
+app.get("/projects/new", (req, res) => {
     res.send("Send the create project page");
 });
 
 // Save the new project from the create form
-app.post("/projects/new",(req,res)=>{
+app.post("/projects/new", (req, res) => {
     res.send("Save the new project");
 });
 
 
 // READ
 // Get all projects
-app.get("/projects",(req,res)=>{
+app.get("/projects", (req, res) => {
     res.send("Send all of the projects");
 });
 
 // Get one project by id
-app.get("/projects/:id",(req,res)=>{
+app.get("/projects/:id", (req, res) => {
     res.send(`Send project ${req.params.id}`);
 });
 
 
 // UPDATE
 // Get the edit page for one project
-app.get("/projects/:id/edit",(req,res)=>{
+app.get("/projects/:id/edit", (req, res) => {
     res.send(`Send the edit page for project ${req.params.id}`);
 });
 
 // Save the edit form for one project
-app.post("/projects/:id/edit",(req,res)=>{
+app.post("/projects/:id/edit", (req, res) => {
     res.send(`Save the edits to project ${req.params.id}`);
 });
 
 
 // DELETE
 // Delete one project by id
-app.post("/projects/:id/delete",(req,res)=>{
+app.post("/projects/:id/delete", (req, res) => {
     res.send(`Delete project ${req.params.id}`);
 });
 
 
 // view all
-app.get("/threads",(req,res)=>{
+app.get("/threads", (req, res) => {
     res.send("This route sends all threads");
 });
 
 // view one
-app.get("/threads/:id",(req,res)=>{
+app.get("/threads/:id", (req, res) => {
     console.log(req.params.id);
     res.send("This route returns thread ", req.params.id);
 });
 
 // create a thread
-app.post("/threads",(req,res)=>{
+app.post("/threads", (req, res) => {
     res.send("POST request called")
 })
 
 // edit a thread
-app.put("/threads/:id",(req,res)=>{
+app.put("/threads/:id", (req, res) => {
     console.log(req.params.id);
     res.send("This route edits thread ", req.params.id);
 })
 
 // delete a thread
-app.delete("/threads/:id",(req,res)=>{
+app.delete("/threads/:id", (req, res) => {
     console.log(req.params.id);
     res.send("This route deletes thread ", req.params.id);
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`App is listening on http://localhost:${PORT}`)
 })
